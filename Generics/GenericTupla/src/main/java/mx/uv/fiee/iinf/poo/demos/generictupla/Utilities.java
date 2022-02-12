ccae3aa3d016b987bb9a44ed8de26fd90cca775c
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2022-02-11 23:16:42 -0600

    Generic Class ad Method Example

--- Generics/GenericTupla/src/main/java/mx/uv/fiee/iinf/poo/demos/generictupla/Utilities.java
+++
@@ -1,8 +0,0 @@
-package mx.uv.fiee.iinf.poo.demos.generictupla;
-
-public class Utilities {
-    
-    public static <T extends GenericTupla> boolean compare (T one, T two) {
-            return one.getKey ().equals (two.getKey ()) && one.getValue ().equals (two.getValue ());
-    }
-}
